d6dac4366bb2c735b9cef9f00973587a4bdc7d3a
Hermes <hermes.espinola@wizeline.com>
2018-01-22 19:50:43 -0600

    init test and code

---
+++ practicas/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "operations.c"
+
+int main() {
+  return 0;
+}
---
+++ practicas/operationstest.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "operations.h"
+
+void testAddition(void);
+void testSubstraction(void);
+void printTestMessage(int, char*, char*);
+
+void printTestMessage(int condition, char *messagePass, char *messageFailed) {
+  if (condition) {
+    printf("%s\n", messagePass);
+  } else {
+    printf("%s\n", messageFailed);
+  }
+}
+
+void testAddition(void) {
+  int a = 5;
+  int b = 7;
+
+  int result = add(a, b);
+  int expected = a + b;
+
+  if (result == expected) {
+    printf("Add test passed\n");
+  } else {
+    fprintf(stderr, "Add test failed\n");
+  }
+}
+
+void testSubstraction(void) {
+  int a = 5;
+  int b = 7;
+
+  int result = substraction(a, b);
+  int expected = a - b;
+
+  printTestMessage(a == b, "Substraction test passed", "Substraction test failed")
+}
+
+int main(int argc, char const *argv[]) {
+  testAddition();
+  testSubstraction();
+}
